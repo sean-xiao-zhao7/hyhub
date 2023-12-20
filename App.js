83ee8dd95552194192c29aad47c5890f825a98a6
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-12-20 17:09:40 -0500

    Remove tab circle

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, DefaultTheme } from "react-native-paper";
 
 import MainBottomTabNavigator from "./src/navigators/MainBottomTabNavigator";
 
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        secondaryContainer: "transparent", // Use transparent to disable the little highlighting oval
+    },
+};
+
 export default function App() {
     return (
-        <PaperProvider>
+        <PaperProvider theme={theme}>
             <SafeAreaProvider>
                 <NavigationContainer>
                     <MainBottomTabNavigator />
