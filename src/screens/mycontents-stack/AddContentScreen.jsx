a10ab5c08be0effb07ed16ec6a102b4df39140ed
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-12-21 13:46:31 -0500

    Add addcontent form

--- src/screens/mycontents-stack/AddContentScreen.jsx
+++ src/screens/mycontents-stack/AddContentScreen.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { Button, Text } from "react-native-paper";
+import { Button, Text, TextInput } from "react-native-paper";
 
 import mainScreenStyle from "../styles/mainScreenStyle";
 import colors from "../../styles/colors";
 import MyDivider from "../../components/MyDivder";
 
 const AddContentScreen = ({ navigation }) => {
+    const [title, setTitle] = useState("");
+    const [date, setDate] = useState("");
+    const [address, setAddress] = useState("");
+    const [description, setDescription] = useState("");
+
     return (
         <SafeAreaView>
             <ScrollView style={mainScreenStyle}>
@@ -33,6 +39,47 @@ const AddContentScreen = ({ navigation }) => {
                         onPress={() => navigation.goBack()}
                     >
                         Cancel
+                    </Button>
+                </View>
+                <MyDivider />
+                <View>
+                    <Text>Full Title</Text>
+                    <MyDivider />
+                    <TextInput
+                        label="Title"
+                        value={title}
+                        onChangeText={(text) => setTitle(text)}
+                    />
+                    <MyDivider />
+                    <Text>Date (if applicable)</Text>
+                    <MyDivider />
+                    <TextInput
+                        label="Date"
+                        value={date}
+                        onChangeText={(text) => setDate(text)}
+                    />
+                    <MyDivider />
+                    <Text>Address / Location / Online</Text>
+                    <MyDivider />
+                    <TextInput
+                        label="Address"
+                        value={address}
+                        onChangeText={(text) => setAddress(text)}
+                    />
+                    <MyDivider />
+                    <Text>Full Description / Details</Text>
+                    <MyDivider />
+                    <TextInput
+                        label="Description"
+                        value={description}
+                        onChangeText={(text) => setDescription(text)}
+                    />
+                    <MyDivider />
+                    <Button
+                        mode="contained"
+                        onPress={() => navigation.goBack()}
+                    >
+                        Add
                     </Button>
                 </View>
             </ScrollView>
