0c953c8a1e951a9f33f6f932eb02fca7e878906c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-12-22 13:39:43 -0500

    SafeInsetsLayoutContainer

---
+++ src/components/SafeInsetsLayoutContainer.jsx
@@ -0,0 +1,7 @@
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const SafeInsetsLayoutContainer = () => {
+    return <View></View>;
+};
+
+export default SafeInsetsLayoutContainer;
