25feb484800d7dab8eb838a0618679ab00cb03d8
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-01-22 16:00:59 -0500

    Improve ContentPreview spacing

--- src/components/ContentPreview.jsx
+++ src/components/ContentPreview.jsx
@@ -1,22 +1,54 @@
 import { Card, Text } from "react-native-paper";
+import colors from "../styles/colors";
 
-const ContentPreview = ({ id, title, date, address, description, padding }) => (
-    <Card
-        mode={padding === "none" ? "contained" : "elevated"}
-        style={{ backgroundColor: "white" }}
-    >
-        <Card.Title title={title} />
-        <Card.Content>
-            <Text variant="titleLarge">{date}</Text>
-            <Text variant="bodyMedium">{address}</Text>
-            <Text variant="bodyMedium">{description}</Text>
-        </Card.Content>
-        {/* <Card.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
-        {/* <Card.Actions>
+const ContentPreview = ({ id, title, date, address, description, padding }) =>
+    padding === "none" ? (
+        <Card
+            mode="contained"
+            style={{
+                backgroundColor: colors.backgroundColorGrayLight,
+                borderRadius: 0,
+            }}
+            contentStyle={{
+                paddingVertical: 10,
+            }}
+        >
+            <Card.Title title={title} />
+            <Card.Content>
+                <Text variant="titleLarge">{date}</Text>
+                <Text variant="bodyMedium">{address}</Text>
+                <Text variant="bodyMedium">{description}</Text>
+            </Card.Content>
+            {/* <Card.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
+            {/* <Card.Actions>
             <Button>Cancel</Button>
             <Button>Ok</Button>
         </Card.Actions> */}
-    </Card>
-);
+        </Card>
+    ) : (
+        <Card
+            mode="outlined"
+            style={{
+                backgroundColor: colors.backgroundColorGrayLight,
+                borderColor: colors.backgroundColorGray,
+                borderRadius: 10,
+            }}
+            contentStyle={{
+                paddingVertical: 10,
+            }}
+        >
+            <Card.Title title={title} />
+            <Card.Content>
+                <Text variant="titleLarge">{date}</Text>
+                <Text variant="bodyMedium">{address}</Text>
+                <Text variant="bodyMedium">{description}</Text>
+            </Card.Content>
+            {/* <Card.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
+            {/* <Card.Actions>
+        <Button>Cancel</Button>
+        <Button>Ok</Button>
+    </Card.Actions> */}
+        </Card>
+    );
 
 export default ContentPreview;
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,7 +1,9 @@
 const colors = {
     mainColor: "#ff7402", // orange
     secondColor: "#00328b", // blue
-    backgroundColor: "white", // light
+    backgroundColor: "white", // white
+    backgroundColorGrayLight: "#f5f6f7",
+    backgroundColorGray: "#dadde1",
     borderColor: "#b7b7b7",
 };
 
