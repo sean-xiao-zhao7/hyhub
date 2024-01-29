40ce07adb731164a3716342c758f34e0dd1e3604
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-01-29 18:14:19 -0500

    Add createAsyncThunk

--- src/redux/slices/myContentsSlice.js
+++ src/redux/slices/myContentsSlice.js
@@ -1,31 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Content from "../../models/content";
+
+export const addContentAction = createAsyncThunk(
+    "content/addContentAction",
+    async (thunkAPI) => {
+        const newContent = new Content(
+            action.payload.title,
+            action.payload.date,
+            action.payload.address,
+            action.payload.description
+        );
+        try {
+            await AsyncStorage.setItem("fire-gem-contents", value);
+        } catch (err) {
+            console.log(err);
+        }
+        return JSON.stringify(newContent);
+    }
+);
+
+export const removeContentAction = createAsyncThunk(
+    "content/removeContentAction",
+    async (thunkAPI) => {
+        state.contents.filter(
+            (content) => content.id !== action.payload.newContent.id
+        );
+    }
+);
 
 const myContentsSlice = createSlice({
     name: "myContents",
     initialState: {
         contents: [],
     },
-    reducers: {
-        addContentAction: (state, action) => {
-            const newContent = new Content(
-                action.payload.title,
-                action.payload.date,
-                action.payload.address,
-                action.payload.description
-            );
-            state.contents.push(JSON.stringify(newContent));
-        },
-        removeContentAction: (state, action) => {
-            state.contents.filter(
-                (content) => content.id !== action.payload.newContent.id
-            );
-        },
+    extraReducers: (builder) => {
+        builder
+            .addCase(addContentAction.fulfilled, (state, action) => {
+                state.contents.push();
+            })
+            .addCase(removeContentAction.fulfilled, (state, action) => {
+                // remove target event from contents
+            });
     },
 });
 
-export const { addContentAction, removeContentAction } =
-    myContentsSlice.actions;
 export default myContentsSlice;
