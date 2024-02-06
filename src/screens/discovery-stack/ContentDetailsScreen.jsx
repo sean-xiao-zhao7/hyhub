146cf7836ed069df89289b9b50c73fcd317ec7eb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-02-06 14:56:12 -0500

    Add content details screen

--- src/screens/discovery-stack/ContentDetailsScreen.jsx
+++ src/screens/discovery-stack/ContentDetailsScreen.jsx
@@ -1,5 +1,4 @@
-import { View, ScrollView } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, ScrollView, Image } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { Button, Text } from "react-native-paper";
 
@@ -11,15 +10,62 @@ import ContentPreview from "../../components/ContentPreview";
 
 import { useSelector } from "react-redux";
 
-const ContentDetailsScreen = (props) => {
+const ContentDetailsScreen = ({ route, navigation }) => {
+    const { content } = route.params;
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <ScrollView contentContainerStyle={mainScreenStyle}>
-                <View>
-                    <Text>Content details</Text>
+        <ScrollView contentContainerStyle={mainScreenStyle}>
+            <View style={{ paddingVertical: 10 }}>
+                <Text style={{ fontSize: 20 }}>{content.title}</Text>
+                <View
+                    style={{
+                        padding: 10,
+                        marginVertical: 10,
+                        borderRadius: 5,
+                        backgroundColor: colors.backgroundColorGrayLight,
+                    }}
+                >
+                    <View
+                        style={{
+                            alignItems: "center",
+                            flexDirection: "row",
+                        }}
+                    >
+                        <MaterialCommunityIcons
+                            name="clock"
+                            size={24}
+                            color={colors.mainColor}
+                            style={{ paddingRight: 10 }}
+                        />
+                        <Text style={{ fontSize: 18 }}>{content.date}</Text>
+                    </View>
+                    <MyDivider />
+                    <View
+                        style={{
+                            alignItems: "center",
+                            flexDirection: "row",
+                        }}
+                    >
+                        <MaterialCommunityIcons
+                            name="map-marker"
+                            size={24}
+                            color={colors.mainColor}
+                            style={{ paddingRight: 10 }}
+                        />
+                        <Text style={{ fontSize: 18 }}>{content.address}</Text>
+                    </View>
+                    <MyDivider />
+                    <Text style={{ fontSize: 18 }}>{content.description}</Text>
+                    <MyDivider />
+                    {/* <Text style={{ color: colors.mainColor }}>
+                        ID: {content.id} (interal)
+                    </Text> */}
                 </View>
-            </ScrollView>
-        </SafeAreaView>
+                <Image
+                    source={{ uri: content.image }}
+                    style={{ height: 300, borderRadius: 5 }}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
