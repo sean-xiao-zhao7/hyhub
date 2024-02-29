ff9cdc5b3251e09ca42cd402321aeff52779895d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-02-29 16:52:39 -0500

    Add user account header

--- src/navigators/stacks/UserTabStack.js
+++ src/navigators/stacks/UserTabStack.js
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import UserHomeScreen from "../../screens/user-stack/UserHomeScreen";
 import { withHeaderOptions } from "../options/headerOptions";
@@ -10,6 +12,25 @@ const UserStack = () => (
         <UserStackNavigator.Screen
             name="UserHomeScreen"
             component={UserHomeScreen}
+            options={{
+                headerTitleAlign: "left",
+                title: "Account",
+                headerRight: (props) => (
+                    <View
+                        style={{
+                            alignItems: "center",
+                            flexDirection: "row",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <MaterialCommunityIcons
+                            name="login"
+                            size={30}
+                            color={"#fff"}
+                        />
+                    </View>
+                ),
+            }}
         />
     </UserStackNavigator.Navigator>
 );
